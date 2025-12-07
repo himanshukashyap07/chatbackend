be13caf95f7f87244f84a0ceb7a48dc67e495be3
himanshu kashyap <kashyapji1908@gmail.com>
2025-12-07 20:35:29 +0530

    add pagination

--- routes/Message.route.js
+++ routes/Message.route.js
@@ -26,16 +26,27 @@ export default function messageRoute(io) {
         try {
             const { u1, u2 } = req.params;
 
+            // Pagination params
+            const page = parseInt(req.query.page) || 1;
+            const limit = parseInt(req.query.limit) || 15;
+
+            // How many messages to skip
+            const skip = (page - 1) * limit;
+
+            // Fetch messages newest → oldest
             const msgs = await Msg.find({
                 $or: [
                     { sender: u1, reciver: u2 },
                     { sender: u2, reciver: u1 }
                 ]
-            }).sort({ createdAt: 1 });
+            })
+                .sort({ createdAt: 1 })
+                .skip(skip)
+                .limit(limit);
 
-            res.json({ success: true, data: msgs });
+            res.json({ success: true, data: finalMsgs });
         } catch (e) {
-            res.status(500).json({ success: false,e, message: "Error" });
+            res.status(500).json({ success: false, message: "Error", e });
         }
     });
 
